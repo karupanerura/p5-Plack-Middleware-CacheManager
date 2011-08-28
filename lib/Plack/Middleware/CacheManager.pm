777f0b90c599ea4d73b111bcf1d46381a57f3e33
Kenta Sato <karupa@cpan.org>
2011-08-28 11:28:00 +0900

    initial commit

---
+++ lib/Plack/Middleware/CacheManager.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::CacheManager;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::CacheManager -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::CacheManager;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::CacheManager is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>karupa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
